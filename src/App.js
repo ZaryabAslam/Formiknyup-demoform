0c5082f4de8e4bdc2a9dd4528c048017dfe90507
Zaryab Aslam <zaryabaslam553@gmail.com>
2021-01-28 14:22:26 +0500

    formik and yup demo from

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Stepper from "./stepper";
+function App() {
+  return (
+    <div className="App">
+     <Stepper/>
+    </div>
+  );
+}
+
+export default App;
